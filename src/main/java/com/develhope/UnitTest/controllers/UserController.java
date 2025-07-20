442dc1fe8e0b3ace1e00c5b004316fdff449c92b
desan <desanctisisabella95@gmail.com>
2025-07-20 21:20:40 +0200

    created dto package with APIResponse class, completed CRUD in Controller class

--- src/main/java/com/develhope/UnitTest/controllers/UserController.java
+++ src/main/java/com/develhope/UnitTest/controllers/UserController.java
@@ -2,13 +2,15 @@ package com.develhope.UnitTest.controllers;
 
 import com.develhope.UnitTest.models.User;
 import com.develhope.UnitTest.repositories.UserRepository;
+import jakarta.validation.Valid;
+import com.develhope.UnitTest.dto.APIResponse;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -28,5 +30,29 @@ public class UserController {
             return ResponseEntity.ok(optionalUser.get());
 
         }
+    }
+    @PostMapping("")
+    public ResponseEntity<APIResponse> create(@Valid @RequestBody User user,
+                       BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            return ResponseEntity.badRequest().body(new APIResponse(bindingResult.getAllErrors()));
+        }else{
+                return ResponseEntity.ok().body(new APIResponse(userRepository.save(user)));
+         }
+
+    }
+    @GetMapping("/users")
+    public List<User> readAll() {
+    return userRepository.findAll();
+    }
+
+    @PutMapping("/{id}")
+    public void updateUser(@PathVariable Integer id, @NotNull @RequestBody User us) {
+        us.setId(id);
+        userRepository.save(us);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        userRepository.deleteById(id);
     }
 }
--- src/main/java/com/develhope/UnitTest/models/User.java
+++ src/main/java/com/develhope/UnitTest/models/User.java
@@ -22,6 +22,6 @@ public class User {
 
     private Integer age;
 
-    private User() {
+    public User() {
     }
 }
